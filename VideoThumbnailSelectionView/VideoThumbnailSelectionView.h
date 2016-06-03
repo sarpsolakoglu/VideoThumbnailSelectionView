a333d2a9b8a4cd8b443443f301278c5a039bcc21
Sarp Solakoğlu <sarpsolakoglu@scorpapp.com>
2016-06-03 11:43:32 +0300

    Initial Commit

---
+++ VideoThumbnailSelectionView/VideoThumbnailSelectionView.h
@@ -0,0 +1,19 @@
+//
+//  VideoThumbnailSelectionView.h
+//  VideoThumbnailSelectionView
+//
+//  Created by Sarp Solakoğlu on 03/06/16.
+//  Copyright © 2016 Sarp Solakoğlu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for VideoThumbnailSelectionView.
+FOUNDATION_EXPORT double VideoThumbnailSelectionViewVersionNumber;
+
+//! Project version string for VideoThumbnailSelectionView.
+FOUNDATION_EXPORT const unsigned char VideoThumbnailSelectionViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <VideoThumbnailSelectionView/PublicHeader.h>
+
+
